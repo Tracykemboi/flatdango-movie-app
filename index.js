9f2654145c1499d069e77408337e9db3520882f3
Tracy Kemboi <tkemboi72@gmail.com>
2024-07-17 12:04:05 +0300

    update the purchase

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 
 function showOneFilm(film){
+    let one=document.querySelector('#first-movie')
+    one.innerHTML=`
+    <img src="${film.poster}">
+    <div class="filmcontent">
+    <h4>
+    ${film.title}
+    </h4>
+    <p>
+    <span class="tickets-Sold">${film.tickets_sold}</span> Tickets sold
+    </p>
+    <p id='remainingtickets'>Available tickets = ${film.capacity-film.tickets_sold}</P>
+    <p>Theatre capacity = ${film.capacity}</p>
+    <p>Show runtime = ${film.runtime}</p>
+    <p>Showtime = ${film.showtime}</p>
+    <p>Movie story line:${film.description}</p>
+    
+    <div> 
+    <button id='PurchaseTicket'> Buy Ticket</button>
+    </div>
+    </div>
+    `
+    const purchaseButton = one.querySelector('#PurchaseTicket');
+    purchaseButton.addEventListener('click', () => {
+        if (film.tickets_sold < film.capacity) {
+            film.tickets_sold += 1;
+            one.querySelector('.tickets-Sold').textContent = film.tickets_sold;
+            one.querySelector('#remainingtickets').textContent = `Available tickets = ${film.capacity - film.tickets_sold}`;
+            updateTicketPurchase();
+        } else {
+            alert('Sorry, all tickets are sold out.');
+        }
+    });
+}
+    // one.querySelector('#PurchaseTicket').addEventListener('click',()=>{
+    //   film.tickets_sold+= 1 
+    //     one.querySelector('span').textContent=film.tickets_sold
+    //     updateTicketPurchase()
+    // })
+
+    }
+
+
+function showAllFilm(film){
 let poster=document.createElement('li')
 poster.className='filmitem'
 poster.innerHTML=`
@@ -24,34 +67,54 @@ ${film.title}
 `
 poster.querySelector('#Purchaseticket').addEventListener('click',()=>{
   film.tickets_sold+= 1 
+  const availableTickets=film.capacity-film.tickets_sold
     poster.querySelector('span').textContent=film.tickets_sold
+    updateTicketPurchase()
 })
 document.querySelector('#films').appendChild(poster)
 }
 
 // fetch requests
-// get fetch for all films
+// get fetch for the first and all films
+function getOneFilm() {
+    fetch('http://localhost:3000/films')
+            .then(res => res.json())
+            .then(films => {
+                // Display the first movie
+                showOneFilm(films[0]);
+})
+}
+
+
 function getAllFilms() {
     fetch('http://localhost:3000/films')
     .then(res=>res.json())
-    .then(films=> films.forEach(film => showOneFilm(film)))
+    .then(films=> films.forEach(film => showAllFilm(film)))
 }
-function updateTicketPurchase(filmObj) {
-    fetch(`http://localhost:3000/films/${film.id}`,{
-        method:'PATCH',
-        headers:{
-            'Content-Type':'application/json-patch+json'
+function updateTicketPurchase(filmsobj) {
+    const filmId = filmsobj.id; 
+
+    fetch(`http://localhost:3000/films/${filmId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
         },
-        body:JSON.stringify(filmObj)
+        body: JSON.stringify(filmId)
     })
-    .then(res=>res.json())
-    .then(films=>console.log(films))
-
+    .then(res => res.json())
+    .then(films=> {
+        console.log('Film updated:', films);
+        // Handle any additional logic after updating the film
+    })
+    .catch(error => {
+        console.error('Error updating film:', error);
+        // Handle errors (e.g., network issues, invalid data, etc.)
+    });
 }
-
 
 function initializeFilms() {
+    getOneFilm()
     return getAllFilms()
-    // films.forEach(film => showOneFilm(film))
+   
 }
 initializeFilms()
